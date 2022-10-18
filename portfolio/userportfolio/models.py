fd68416e8c4c31b57908eb1116f6bf7cf3748d2c
Jacob Riba <jacobriba7@gmail.com>
2022-10-18 21:19:57 +0200

    Added user model

--- portfolio/userportfolio/models.py
+++ portfolio/userportfolio/models.py
@@ -1,3 +1,23 @@
+import geocoder
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+
+
+mapbox_access_token = ''
 
 # Create your models here.
+class AppUsers(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) 
+    home_address = models.TextField()
+    lat = models.FloatField(blank=True, null=True)
+    long = models.FloatField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        g = geocoder.mapbox(self.address, key=mapbox_access_token)
+        g = g.latlng  # returns => [lat, long]
+        self.lat = g[0]
+        self.long = g[1]
+        return super(AppUsers, self).save(*args, **kwargs)

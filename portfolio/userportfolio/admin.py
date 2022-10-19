70da25ca1567d115147abb9a75b3bedf3cd8f9ba
Jacob Riba <jacobriba7@gmail.com>
2022-10-19 10:43:03 +0200

    Created user model and added initial migrations

--- portfolio/userportfolio/admin.py
+++ portfolio/userportfolio/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import AppUsers 
 
 # Register your models here.
+
+
+# Define an inline admin descriptor for AppUsers model
+# which acts a bit like a singleton
+class UsersInline(admin.StackedInline):
+    model = AppUsers
+    can_delete = False
+    verbose_name_plural = 'Users'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = [UsersInline]
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
